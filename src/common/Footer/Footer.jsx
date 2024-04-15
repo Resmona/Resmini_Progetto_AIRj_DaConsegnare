d8535fe84eb52629bcee5aa686cb467ac26124f3
ResmoPapaV <161448465+Resmona@users.noreply.github.com>
2024-04-15 12:40:49 +0200

    Add files via upload

---
+++ src/common/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+import logo from "../../logo_grigio_AIRj.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='wrapper'>
+        <div className="footer-column">
+          <p>&copy; 2024 AIRj - Tutti i diritti riservati</p>
+          <p>questo progetto non è in nessun modo affiliato alle marche soprastanti</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div id="contact" className='wrapper'>
+      <h2>La nostra selezione è rigorosamente curata per includere solamente le marche leader nel settore, riconosciute per il loro impegno costante nell'innovazione e nella performance. Offriamo soluzioni avanzate per ogni necessità del giocatore, dalla ammortizzazione e supporto alla caviglia fino all'aderenza. La nostra offerta si distingue anche per lo stile, con una gamma che spazia dalle edizioni limitate alle collaborazioni esclusive, permettendo ai giocatori di esprimere la propria passione per il basket sia in campo che nella vita quotidiana. La varietà del nostro catalogo assicura una corrispondenza perfetta per ogni tipo di giocatore, garantendo leggerezza, durabilità e comfort. Impegnati nella soddisfazione del cliente, promettiamo un'esperienza d'acquisto eccellente, caratterizzata da un servizio clienti amichevole, spedizioni rapide e una procedura di reso semplificata. Perchè la vostra soddisffazione è il nostro canestro da tre punti. Le spedizioni vengono effettuate entro le 24/48 ore lavorative, anche per gli appassionati dell'ultimo secondo, gratuita per ordini sopra i 200 €</h2>
+    </div>
+  );
+}
+
+export default Contact;
